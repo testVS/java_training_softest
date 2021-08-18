a18a28b0289f0ddb7fd52c0176333f8e509a863d
testVS <55035900+testVS@users.noreply.github.com>
2021-08-18 18:49:26 +0300

    edited First.java file, recompiled with gradle

--- sandbild/src/main/java/First.java
+++ sandbild/src/main/java/First.java
@@ -1,6 +1,6 @@
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world") ;
+		System.out.println("Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, Hello world, ") ;
  }
 }
